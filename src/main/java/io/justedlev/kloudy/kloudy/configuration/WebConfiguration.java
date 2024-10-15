3dd4f0d8614d3f62a220107fab1a9d6524b5b3c2
Justedlev <edyardlyk@gmail.com>
2024-10-16 00:54:22 +0300

    chore: update WebConfiguration

--- src/main/java/io/justedlev/kloudy/kloudy/configuration/WebConfiguration.java
+++ src/main/java/io/justedlev/kloudy/kloudy/configuration/WebConfiguration.java
@@ -2,35 +2,29 @@ package io.justedlev.kloudy.kloudy.configuration;
 
 import lombok.NonNull;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+    private static final String ALL = "*";
+
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry
                 .addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns(ALL)
                 .allowCredentials(true)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
                         HttpMethod.PATCH.name(),
-                        HttpMethod.DELETE.name()
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
                 )
-                .allowedHeaders(
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
-                        HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN
-                )
-                .exposedHeaders(
-                        HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS
-                );
+                .allowedHeaders(ALL);
     }
 }
