2d4d3a065f5b49da9685c0ccdcc301473cd7c027
Edward L. <edward.lukashevits@gmail.com>
2025-09-14 22:53:59 +0300

    feat: add `FileMetadataAttributeNames.java`
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

---
+++ src/main/java/io/justedlev/msrv/kloudy/repository/entity/FileMetadataAttributeNames.java
@@ -0,0 +1,8 @@
+package io.justedlev.msrv.kloudy.repository.entity;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class FileMetadataAttributeNames {
+    public static final String SHA_256 = "sha256";
+}
