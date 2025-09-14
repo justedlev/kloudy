5c86b0c5908aa8df37a9119f5990af8e9d602016
Edward L. <edward.lukashevits@gmail.com>
2025-09-14 22:49:25 +0300

    refactor(config): update `SecurityConfiguration.java`
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/kloudy/configuration/SecurityConfiguration.java
+++ src/main/java/io/justedlev/msrv/kloudy/configuration/SecurityConfiguration.java
@@ -1,79 +1,105 @@
 package io.justedlev.msrv.kloudy.configuration;
 
-import io.justedlev.msrv.kloudy.configuration.properties.SecurityProperties;
+import io.justedlev.msrv.kloudy.configuration.properties.KloudySecurityConfigurationProperties;
 import io.justedlev.msrv.kloudy.controller.FilesController;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.context.properties.PropertyMapper;
+import org.springframework.boot.web.client.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.Environment;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.DelegatingJwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.ExpressionJwtGrantedAuthoritiesConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 import java.util.Collection;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+@EnableConfigurationProperties(KloudySecurityConfigurationProperties.class)
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private static final ExpressionParser PARSER = new SpelExpressionParser();
     private static final PropertyMapper PROPERTY_MAPPER = PropertyMapper.get().alwaysApplyingWhenNonNull();
     private static final String ROLE_PREFIX = "ROLE_";
+    private static final String SCOPE_PREFIX = "SCOPE_";
 
-    private final SecurityProperties properties;
+    private final KloudySecurityConfigurationProperties kscp;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(@NonNull HttpSecurity httpSecurity) throws Exception {
+    SecurityFilterChain securityFilterChain(@NonNull HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(registry -> {
-                    properties.getWhitelist().forEach((k, v) -> registry.requestMatchers(k, v).permitAll());
+                    registry.requestMatchers(HttpMethod.GET, "/actuator/prometheus")
+                            .hasAuthority(SCOPE_PREFIX + "prometheus.metrics:read");
                     var filesPath = FilesController.CONTEXT_PATH + "/**";
                     registry.requestMatchers(HttpMethod.GET, filesPath)
                             .hasAnyAuthority(
-                                    "SCOPE_kloudy.files:read",
-                                    "ROLE_user"
+                                    SCOPE_PREFIX + "kloudy.files:ro",
+                                    ROLE_PREFIX + "user"
                             );
                     registry.requestMatchers(HttpMethod.POST, filesPath)
                             .hasAnyAuthority(
-                                    "SCOPE_kloudy.files:write",
-                                    "ROLE_user"
+                                    SCOPE_PREFIX + "kloudy.files:rw",
+                                    ROLE_PREFIX + "user"
                             );
                     registry.requestMatchers(HttpMethod.DELETE, filesPath)
                             .hasAnyAuthority(
-                                    "ROLE_admin"
+                                    SCOPE_PREFIX + "kloudy.files:rw",
+                                    ROLE_PREFIX + "admin"
                             );
+                    kscp.getWhitelist().forEach((k, v) -> registry.requestMatchers(k, v).permitAll());
                     registry.anyRequest().authenticated();
                 })
                 .build();
     }
 
     @Bean
-    SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    ClientHttpRequestInterceptor oauth2ClientHttpRequestInterceptor(
+            OAuth2AuthorizedClientManager authorizedClientManager,
+            Environment env
+    ) {
+        var interceptor = new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+        interceptor.setClientRegistrationIdResolver(request -> env.getProperty("spring.application.name"));
+
+        return interceptor;
+    }
+
+    @Bean
+    RestClientCustomizer configClientOAuth2Customizer(ClientHttpRequestInterceptor oauth2ClientHttpRequestInterceptor) {
+        return builder -> builder.requestInterceptor(oauth2ClientHttpRequestInterceptor);
+    }
+
+    @Bean
+    OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository registrationRepository,
+            OAuth2AuthorizedClientService clientService
+    ) {
+        return new AuthorizedClientServiceOAuth2AuthorizedClientManager(registrationRepository, clientService);
     }
 
     @Bean
