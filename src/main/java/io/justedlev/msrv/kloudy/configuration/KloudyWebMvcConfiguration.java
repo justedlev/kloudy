5e1e874988d4e8db5df7995e600e85a3bf22b53e
Edward L. <edward.lukashevits@gmail.com>
2025-09-14 23:04:16 +0300

    refactor: rename `WebConfiguration` to `KloudyWebMvcConfiguration`
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/kloudy/configuration/KloudyWebMvcConfiguration.java
+++ src/main/java/io/justedlev/msrv/kloudy/configuration/KloudyWebMvcConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class KloudyWebMvcConfiguration implements WebMvcConfigurer {
     private static final String ALL = "*";
 
     @Override
