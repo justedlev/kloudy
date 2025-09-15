139672dc63fbf917cc903a62dbc479e3f4d8959a
Edward L. <edward.lukashevits@gmail.com>
2025-09-15 10:43:33 +0300

    refactor: update `FileMetadata.java`
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/kloudy/repository/entity/FileMetadata.java
+++ src/main/java/io/justedlev/msrv/kloudy/repository/entity/FileMetadata.java
@@ -1,51 +1,52 @@
 package io.justedlev.msrv.kloudy.repository.entity;
 
-import io.justedlev.sb3c.DefaultAuditable;
-import io.justedlev.sb3c.DefaultPersistable;
-import io.justedlev.sb3c.DefaultVersionable;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import io.justedlev.msrv.kloudy.model.Attributable;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.envers.AuditOverride;
 import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+import org.springframework.http.MediaType;
 
 import java.io.Serializable;
+import java.util.Map;
 import java.util.UUID;
 
 @Getter
 @Setter
-@With
-@SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Audited
-@AuditOverride(forClass = DefaultVersionable.class)
+@AuditOverride(forClass = NumeratedVersionable.class)
 @AuditOverride(forClass = DefaultAuditable.class)
-@AuditOverride(forClass = DefaultPersistable.class)
+@AuditOverride(forClass = AbstractPersistable.class)
 @Entity
 @DynamicUpdate
 @Table(name = "file_metadata")
-@ToString
-public class FileMetadata extends DefaultVersionable<UUID> implements Serializable {
-
+public class FileMetadata extends NumeratedVersionable<UUID> implements Attributable<String>, Serializable { // NOSONAR
     @NotBlank
     @Column(name = "filename", nullable = false)
     private String filename;
-
     @Column(name = "extension")
     private String extension;
-
-    @NotBlank
-    @Column(name = "type", nullable = false)
-    private String type;
-
+    @NotNull
+    @Column(name = "content_type", nullable = false)
+    private MediaType contentType;
     @Min(1)
     @Column(name = "length", nullable = false)
     private Long length;
-
+    @Singular
+    @Fetch(FetchMode.SUBSELECT)
+    @ElementCollection
+    @MapKeyColumn(name = "name")
+    @Column(name = "_value", nullable = false)
+    @CollectionTable(name = "file_metadata_attributes", joinColumns = @JoinColumn(name = "file_metadata_id"))
+    private Map<@NotBlank String, @NotNull String> attributes;
 }
