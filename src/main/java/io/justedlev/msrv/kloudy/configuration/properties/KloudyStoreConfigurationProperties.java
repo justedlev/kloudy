e2c6ec3f480412582bfae0a0a0478fb76eb94e40
Edward L. <edward.lukashevits@gmail.com>
2025-10-18 18:14:16 +0300

    refactor(config): move default bucker dir init to properties
    Signed-off-by: Edward L. <edward.lukashevits@gmail.com>

--- src/main/java/io/justedlev/msrv/kloudy/configuration/properties/KloudyStoreConfigurationProperties.java
+++ src/main/java/io/justedlev/msrv/kloudy/configuration/properties/KloudyStoreConfigurationProperties.java
@@ -3,7 +3,6 @@ package io.justedlev.msrv.kloudy.configuration.properties;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.nio.file.Path;
@@ -15,10 +14,8 @@ public class KloudyStoreConfigurationProperties {
     public static final String PREFIX = KloudyConfigurationProperties.PREFIX + ".store";
     /**
      * The location in filesystem for content
-     * <p>
-     * Default: ${user.home}/.kloudy.bucket.d
      */
-    @Setter(onMethod_ = @Value("${user.home}/.kloudy.bucket.d"), onParam_ = @NonNull) // NOSONAR
+    @Setter(onParam_ = @NonNull) // NOSONAR
     private Path bucket;
     /**
      * The posix permissions
